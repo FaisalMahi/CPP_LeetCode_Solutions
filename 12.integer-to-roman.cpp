fd41f1a684ff28d3b48aac349bdf5a54fdb32081
FaisalMahi <faisalibnaziz39@gmail.com>
2025-06-04 12:12:32 +0600

    Initial Commit

--- 12.integer-to-roman.cpp
+++ 12.integer-to-roman.cpp
@@ -5,11 +5,26 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string intToRoman(int num) {
-        
+    string intToRoman(int num)
+    {
+        vector<int> val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> sym = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string res = "";
+        for (int i = 0; i < 13; i++)
+        {
+            if (num == 0)
+                break;
+            int time = num / val[i];
+            while (time--)
+            {
+                res += sym[i];
+            }
+            num = num % val[i];
+        }
+        return res;
     }
 };
 // @lc code=end
-
