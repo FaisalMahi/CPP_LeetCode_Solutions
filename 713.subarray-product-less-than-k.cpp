8e15bbc38193d0090cf378ff66b2d32b91ec8618
FaisalMahi <faisalibnaziz39@gmail.com>
2025-06-04 18:56:38 +0600

    Initial Commit

---
+++ 713.subarray-product-less-than-k.cpp
@@ -0,0 +1,35 @@
+//Question Link: https://leetcode.com/problems/subarray-product-less-than-k
+
+
+/*
+ * @lc app=leetcode id=713 lang=cpp
+ *
+ * [713] Subarray Product Less Than K
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int numSubarrayProductLessThanK(vector<int> &nums, int k)
+    {
+        if (k <= 1)
+        {
+            return 0;
+        }
+        int i = 0, j = 0, prod = 1, cnt = 0;
+        while (j < nums.size())
+        {
+            prod *= nums[j];
+            while (prod >= k)
+            {
+                prod /= nums[i];
+                i++;
+            }
+            cnt += (j - i + 1);
+            j++;
+        }
+        return cnt;
+    }
+};
+// @lc code=end
