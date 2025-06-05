58e6f24856d23891dadaefd45940467eebc9715a
FaisalMahi <faisalibnaziz39@gmail.com>
2025-06-05 15:37:16 +0600

    Initial Commit

---
+++ 217.contains-duplicate.cpp
@@ -0,0 +1,28 @@
+//Question Link: https://leetcode.com/problems/contains-duplicate
+
+
+/*
+ * @lc app=leetcode id=217 lang=cpp
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
