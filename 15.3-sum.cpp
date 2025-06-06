19ee03f5b62c417665596245a847b58f5e2db14c
FaisalMahi <faisalibnaziz39@gmail.com>
2025-06-06 15:44:07 +0600

    Initial Commit

---
+++ 15.3-sum.cpp
@@ -0,0 +1,58 @@
+//Question Link: https://leetcode.com/problems/3sum
+
+
+/*
+ * @lc app=leetcode id=15 lang=cpp
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<vector<int>> results;
+    void twoSum(vector<int> &nums, int target, int i, int j)
+    {
+        while (i < j)
+        {
+            if (nums[i] + nums[j] > target)
+                j--;
+            else if (nums[i] + nums[j] < target)
+                i++;
+            else
+            {
+                while (i < j && nums[i] == nums[i + 1])
+                    i++;
+                while (i < j && nums[j] == nums[j - 1])
+                    j--;
+                results.push_back({-target, nums[i], nums[j]});
+                i++;
+                j--;
+            }
+        }
+    }
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n < 3)
+        {
+            return {};
+        }
+        results.clear();
+        sort(begin(nums), end(nums));
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            int n1 = nums[i];
+            int target = -n1;
+            twoSum(nums, target, i + 1, n - 1);
+        }
+        return results;
+    }
+};
+// @lc code=end
